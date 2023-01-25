df2e804db336adb3832115aa9eb3cbd50e92d72b
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-01-25 17:04:58 -0600

    test

---
+++ script.js
@@ -0,0 +1,45 @@
+const a = 42.78
+const b = 1.1
+const c = -2
+const d = .5
+const e = 16
+
+//console.log(Math.ceil(42.78));
+//console.log(Math.floor(1.1));
+//console.log(Math.abs(42.78-1.1));
+//console.log(Math.max(a,b,c,d,e))
+//console.log(Math.sqrt(e))
+//console.log(Math.pow(e, d))
+//const dice = Math.floor(Math.random() * 6) + 1
+//console.log(dice)
+
+ let firstVariable = "Hello World"
+ firstVariable = 5
+ firstVariable = secondVariable
+ secondVariable = ""
+
+ yourName = "tyler"
+ console.log(`Hello my name is ${yourName}`)
+
+
+ let age = 40
+ 
+ if (age >= 16) {
+  console.log("Here are the keys")
+ } else if(age < 16) {
+  console.log("sorry, your're too young")
+ }
+
+for (let i = 0; i < 11; i++) {
+  console.log(i)
+}
+
+for (let i = 500; i > 50; i--) {
+
+}
+
+for (let i = 0; i <= 100; i++) {
+   if (i % 2 !=0) {
+    console.log(i)
+   }
+}
